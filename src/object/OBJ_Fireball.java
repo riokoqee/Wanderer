42fb2b5715de1d2fd4ece474a0ebe603e04abe46
riokoqee <rioko988@gmail.com>
2026-02-19 00:08:57 +0500

    feat: projectiles beginning

---
+++ src/object/OBJ_Fireball.java
@@ -0,0 +1,11 @@
+package object;
+
+import entity.Projectile;
+import main.GamePanel;
+
+public class OBJ_Fireball extends Projectile {
+
+    public OBJ_Fireball(GamePanel gp) {
+        super(gp);
+    }
+}
